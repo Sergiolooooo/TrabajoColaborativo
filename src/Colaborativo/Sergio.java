f3331100aea02eaebd978e3a983473f635c7f91f
Sergio Lopez Morales <sergiolopezm098@gmail.com>
2022-08-04 16:34:34 -0600

    Se crea la clase Sergio

---
+++ src/Colaborativo/Sergio.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Colaborativo;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Sergio {
+    
+}
